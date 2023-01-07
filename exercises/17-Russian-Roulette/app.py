5944c0d9ecc162a42fa9be4565c09cb4e10abb0c
Jose L. Gonzalez Diaz <kbcerverjo@gmail.com>
2023-01-07 00:01:43 +0000

    Completed Exercises

--- exercises/17-Russian-Roulette/app.py
+++ exercises/17-Russian-Roulette/app.py
@@ -8,10 +8,11 @@ def spin_chamber():
 
 #  DON'T CHANGE THE CODE ABOVE
 def fire_gun():
-	# YOUR CODE HERE
-	return None
-
-
+	mipo = spin_chamber()
+	if mipo == bullet_position:
+		return "You are dead!"
+	else:
+		return "Keep playing!"
 
 
 print(fire_gun())
--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,17 @@
-# Your code here!!
+def sing():
+
+    song = ""
+    for i in range(12):
+        if i < 4:
+            song += "let it be, "
+        elif i == 4:
+            song += "whisper words of wisdom, "
+        elif i < 10:
+            song += "let it be, "
+        elif i == 10:
+            song += "there will be an answer, "
+        elif i > 10:
+            song += "let it be"
+    return song
+
+print(sing())
